7666fac7a3c86035e2727b252cc6812f7efd7147
Keith Jackson <keith@ministryotech.co.uk>
2016-07-22 16:26:48 +0100

    Helper added for HttpResponseMessages - Will probably supersede the PylonApiController classes

---
+++ Umbraco.PylonLite/HttpResponseMessageGenerator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+
+namespace Umbraco.PylonLite
+{
+    /// <summary>
+    /// Generates standard HttpResponseMessages depending on whether an item is null or not.
+    /// </summary>
+    public static class HttpResponseMessageGenerator
+    {
+        private const string NotFoundMessage = "An item was not found at this address.";
+
+        /// <summary>
+        /// Returns an HttpResponseMessage for a function.
+        /// </summary>
+        /// <param name="function">The function.</param>
+        /// <returns>
+        /// OK, NotFound or Error as appropriate.
+        /// </returns>
+        public static HttpResponseMessage For(HttpRequestMessage request, Func<object> function)
+        {
+            try
+            {
+                var returnItem = function();
+                return returnItem != null
+                        ? request.CreateResponse(HttpStatusCode.OK, returnItem)
+                        : request.CreateErrorResponse(HttpStatusCode.NotFound, NotFoundMessage);
+            }
+            catch (Exception ex)
+            {
+                return request.CreateErrorResponse(HttpStatusCode.InternalServerError, ex.Message);
+            }
+        }
+    }
+}

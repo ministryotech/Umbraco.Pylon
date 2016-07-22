23f00f8bdda9f8ebd87576740a8e96c4532030bf
Keith Jackson <keith@ministryotech.co.uk>
2016-07-22 18:01:27 +0100

    Added FormsAuthWrapper

---
+++ Umbraco.PylonLite/FormsAuthWrapper.cs
@@ -0,0 +1,29 @@
+﻿using System.Web.Security;
+
+namespace Umbraco.PylonLite
+{
+    /// <summary>
+    /// Wraps basic Forms Authentication functions.
+    /// </summary>
+    public class FormsAuthWrapper : IFormsAuthWrapper
+    {
+        /// <summary>
+        /// Sets the authentication cookie.
+        /// </summary>
+        /// <param name="username">The username.</param>
+        /// <param name="createPersistentCookie">if set to <c>true</c> [create persistent cookie].</param>
+        public void SetAuthCookie(string username, bool createPersistentCookie)
+        {
+            FormsAuthentication.SetAuthCookie(username, createPersistentCookie);
+        }
+
+
+        /// <summary>
+        /// Signs out.
+        /// </summary>
+        public void SignOut()
+        {
+            FormsAuthentication.SignOut();
+        }
+    }
+}
---
+++ Umbraco.PylonLite/IFormsAuthWrapper.cs
@@ -0,0 +1,21 @@
+﻿namespace Umbraco.PylonLite
+{
+    /// <summary>
+    /// Wraps basic Forms Authentication functions.
+    /// </summary>
+    public interface IFormsAuthWrapper
+    {
+        /// <summary>
+        /// Sets the authentication cookie.
+        /// </summary>
+        /// <param name="username">The username.</param>
+        /// <param name="createPersistentCookie">if set to <c>true</c> [create persistent cookie].</param>
+        void SetAuthCookie(string username, bool createPersistentCookie);
+
+
+        /// <summary>
+        /// Signs out.
+        /// </summary>
+        void SignOut();
+    }
+}

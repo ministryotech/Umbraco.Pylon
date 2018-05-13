b119eb3d9dbe675c53d0b6d3aeddb41e6b33b9bf
Keith Jackson <keith@ministryotech.co.uk>
2018-05-13 14:38:21 +0100

    Umbraco 7.6.13 compatible pylon version

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -27,5 +27,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("7.5.14.0")]
-[assembly: AssemblyFileVersion("7.5.14.0")]
+[assembly: AssemblyVersion("7.6.13.0")]
+[assembly: AssemblyFileVersion("7.6.13.0")]

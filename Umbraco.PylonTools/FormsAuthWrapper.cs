4dc8a2c3a82a20e7b3f299538f3bff45f50a1054
Keith Jackson <keith@ministryotech.co.uk>
2018-06-08 20:41:56 +0100

    Package updates

---
+++ Umbraco.PylonTools/FormsAuthWrapper.cs
@@ -0,0 +1,52 @@
+﻿using System.Web.Security;
+
+namespace Umbraco.PylonTools
+{
+    #region | Interface |
+
+    /// <summary>
+    /// Wraps basic Forms Authentication functions.
+    /// </summary>
+    public interface IFormsAuthWrapper
+    {
+        /// <summary>
+        /// Sets the authentication cookie.
+        /// </summary>
+        /// <param name="username">The username.</param>
+        /// <param name="createPersistentCookie">if set to <c>true</c> [create persistent cookie].</param>
+        void SetAuthCookie(string username, bool createPersistentCookie);
+
+
+        /// <summary>
+        /// Signs out.
+        /// </summary>
+        void SignOut();
+    }
+
+    #endregion
+
+    /// <summary>
+    /// Wraps basic Forms Authentication functions.
+    /// </summary>
+    public class FormsAuthWrapper : IFormsAuthWrapper
+    {
+        /// <summary>
+        /// Sets the authentication cookie.
+        /// </summary>
+        /// <param name="username">The username.</param>
+        /// <param name="createPersistentCookie">if set to <c>true</c> [create persistent cookie].</param>
+        public void SetAuthCookie(string username, bool createPersistentCookie)
+        {
+            FormsAuthentication.SetAuthCookie(username, createPersistentCookie);
+        }
+
+
+        /// <summary>
+        /// Signs out.
+        /// </summary>
+        public void SignOut()
+        {
+            FormsAuthentication.SignOut();
+        }
+    }
+}
---
+++ Umbraco.PylonTools/ViewStringRenderer.cs
@@ -0,0 +1,83 @@
+﻿using System.IO;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Umbraco.PylonTools
+{
+    #region | Interface |
+
+    /// <summary>
+    /// Converts Views to strings.
+    /// </summary>
+    public interface IViewStringRenderer
+    {
+        /// <summary>
+        /// Renders the specified view to a string.
+        /// </summary>
+        /// <param name="viewName">Name of the view.</param>
+        /// <param name="model">The model.</param>
+        /// <param name="caller">The caller.</param>
+        /// <returns></returns>
+        string Render(string viewName, object model, Controller caller);
+    }
+
+    #endregion
+
+    /// <summary>
+    /// Converts Views to strings.
+    /// </summary>
+    public class ViewStringRenderer : IViewStringRenderer
+    {
+        /// <summary>
+        /// Renders the specified view to a string.
+        /// </summary>
+        /// <param name="viewName">Name of the view.</param>
+        /// <param name="model">The model.</param>
+        /// <param name="caller">The calling controller.</param>
+        /// <returns></returns>
+        public string Render(string viewName, object model, Controller caller)
+        {
+            caller.ViewData.Model = model;
+            SetDefaultContext(caller);
+
+            using (var sw = new StringWriter())
+            {
+                var viewResult = ViewEngines.Engines.FindPartialView(caller.ControllerContext, viewName);
+                var viewContext = new ViewContext(caller.ControllerContext, viewResult.View, caller.ViewData,
+                    caller.TempData, sw);
+                viewResult.View.Render(viewContext, sw);
+                viewResult.ViewEngine.ReleaseView(caller.ControllerContext, viewResult.View);
+
+                return sw.GetStringBuilder().ToString();
+            }
+        }
+
+        #region | Private Methods |
+
+        /// <summary>
+        /// Sets a default context if none is present on the controller.
+        /// </summary>
+        /// <param name="controller">The controller.</param>
+        private void SetDefaultContext(Controller controller)
+        {
+            if (controller.ControllerContext != null)
+                return;
+
+            HttpContextBase wrapper = null;
+            if (HttpContext.Current != null)
+                wrapper = new HttpContextWrapper(HttpContext.Current);
+
+            var routeData = new RouteData();
+
+            if (!routeData.Values.ContainsKey("controller") && !routeData.Values.ContainsKey("Controller"))
+                routeData.Values.Add("controller", controller.GetType().Name
+                                                            .ToLower()
+                                                            .Replace("controller", ""));
+
+            controller.ControllerContext = new ControllerContext(wrapper, routeData, controller);
+        }
+
+        #endregion
+    }
+}

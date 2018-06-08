75fa6524dd2fa46c1b451415c06e6e736bc0741f
Keith Jackson <keith@ministryotech.co.uk>
2018-06-08 12:01:15 +0100

    New projects created

---
+++ Umbraco.PylonTools/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Umbraco.PylonTools.Tests")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a6de1436-3748-4181-bce2-062d1b581b2d")]

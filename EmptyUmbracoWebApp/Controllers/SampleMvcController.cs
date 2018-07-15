0798ccdfe088ecc05acef47319eb70104460eca4
Keith Jackson <keith@ministryotech.co.uk>
2018-07-15 12:50:44 +0100

    Sample Project code

---
+++ EmptyUmbracoWebApp/Controllers/SampleMvcController.cs
@@ -0,0 +1,27 @@
+﻿using EmptyUmbracoWebApp.Repositories;
+using Umbraco.Pylon.Controllers;
+
+namespace EmptyUmbracoWebApp.Controllers
+{
+    /// <summary>
+    /// Base class for an MVC controller, to use if you want to replace Umbraco's default behaviour for a content route or you need a route in the site with no content.
+    /// </summary>
+    /// <remarks>
+    /// Consider, before using this, whether a Surface Controller is actually more appropriate.
+    /// </remarks>
+    /// <seealso cref="Umbraco.Pylon.Controllers.PylonMvcController{ISampleSite, ISamplePublishedContentRepository}" />
+    public abstract class SampleMvcController : PylonMvcController<ISampleSite, ISamplePublishedContentRepository>
+    {
+        #region | Construction |
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SampleMvcController" /> class.
+        /// </summary>
+        /// <param name="site">The site.</param>
+        protected SampleMvcController(ISampleSite site)
+            : base(site)
+        { }
+
+        #endregion
+    }
+}

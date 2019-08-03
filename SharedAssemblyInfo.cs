0f7c14d1610a274a4494721e0c76f9fe58b8db04
Keith Jackson <keith@ministryotech.co.uk>
2019-08-03 09:35:36 +0100

    Umbraco 7.12

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("The Ministry of Technology")]
 [assembly: AssemblyProduct("Umbraco Pylon (Ministry)")]
-[assembly: AssemblyCopyright("Copyright © 2018 Minotech Ltd.")]
+[assembly: AssemblyCopyright("Copyright © 2019 Minotech Ltd.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -27,5 +27,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.711.0.0")]
-[assembly: AssemblyFileVersion("3.711.0.0")]
+[assembly: AssemblyVersion("3.712.0.0")]
+[assembly: AssemblyFileVersion("3.712.0.0")]
